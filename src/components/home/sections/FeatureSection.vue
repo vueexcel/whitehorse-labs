f885b4af3a9e376708f91756fa0db71b51e583ed
vueexcel <vue.excel2011@gmail.com>
2024-07-18 18:48:48 +0530

    fixed image issue.

--- src/components/home/sections/FeatureSection.vue
+++ src/components/home/sections/FeatureSection.vue
@@ -2,36 +2,20 @@
   <section class="relative flex items-center justify-center w-full h-screen bg-black">
     <div class="absolute top-0 left-0 w-full h-full">
       <div class="relative z-10 flex w-full h-full select-none">
+        <img :src="Image1" alt="images" class="absolute top-0 left-0 pointer-events-none" />
+        <img :src="Image2" alt="images" class="absolute top-0 right-0 pointer-events-none" />
         <img
-          src="../../../assets/images/home/image-1.png"
-          alt="images"
-          class="absolute top-0 left-0 pointer-events-none"
-        />
-        <img
-          src="../../../assets/images/home/image-2.png"
-          alt="images"
-          class="absolute top-0 right-0 pointer-events-none"
-        />
-        <img
-          src="../../../assets/images/home/image-3.png"
+          :src="Image3"
           alt="images"
           class="absolute pointer-events-none top-[50%] h-52 left-0 translate-y-[-20%]"
         />
         <img
-          src="../../../assets/images/home/image-5.png"
+          :src="Image5"
           alt="images"
           class="absolute pointer-events-none h-52 right-0 top-[50%] translate-y-[-20%]"
         />
-        <img
-          src="../../../assets/images/home/image-6.png"
-          alt="images"
-          class="absolute bottom-0 pointer-events-none left-[20%]"
-        />
-        <img
-          src="../../../assets/images/home/image-7.png"
-          alt="images"
-          class="absolute bottom-0 pointer-events-none right-[20%]"
-        />
+        <img :src="Image6" alt="images" class="absolute bottom-0 pointer-events-none left-[20%]" />
+        <img :src="Image7" alt="images" class="absolute bottom-0 pointer-events-none right-[20%]" />
       </div>
     </div>
     <p class="text-white text-center max-w-[470px] text-3xl z-20">
@@ -40,3 +24,12 @@
     </p>
   </section>
 </template>
+
+<script setup lang="ts">
+import Image1 from '@/assets/images/home/Image-1.png'
+import Image2 from '@/assets/images/home/Image-2.png'
+import Image3 from '@/assets/images/home/Image-3.png'
+import Image5 from '@/assets/images/home/Image-5.png'
+import Image6 from '@/assets/images/home/Image-6.png'
+import Image7 from '@/assets/images/home/Image-7.png'
+</script>
