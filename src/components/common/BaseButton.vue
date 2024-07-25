ff71ea4b1b311e5d9e51c7ebe9b8fd00e75d319c
vueexcel <vue.excel2011@gmail.com>
2024-07-25 15:24:57 +0530

    removed extra svg icon

--- src/components/common/BaseButton.vue
+++ src/components/common/BaseButton.vue
@@ -5,39 +5,14 @@
   >
     <slot>
       <span>{{ title }}</span>
-      <svg
-        width="12"
-        height="12"
-        viewBox="0 0 13.484375 13.5"
-        version="1.1"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        xmlns="http://www.w3.org/2000/svg"
-        class="ml-3"
-      >
-        <g id="Group-5">
-          <path
-            d="M0 0L12.4843 0L12.4843 12.5"
-            id="Stroke-1"
-            fill="none"
-            fill-rule="evenodd"
-            stroke="black"
-            stroke-width="1"
-          />
-          <path
-            d="M1.17367e-13 12.4999L12.4785 0.00415039"
-            id="Stroke-3"
-            fill="none"
-            fill-rule="evenodd"
-            stroke="black"
-            stroke-width="1"
-          />
-        </g>
-      </svg>
+      <ArrowIcon type="top-right" :path-class="'stroke-black'" class="ml-3 size-5" />
     </slot>
   </button>
 </template>
 
 <script setup lang="ts">
+import ArrowIcon from '@/components/icons/ArrowIcon.vue'
+
 defineProps<{
   title?: string
 }>()
