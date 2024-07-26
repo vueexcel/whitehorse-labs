b21dcd62aabb701eb93a75cb8da0c2dcd2d908bd
vueexcel <vue.excel2011@gmail.com>
2024-07-26 10:52:05 +0530

    correct 'about' page folder structure.

---
+++ src/components/pages/about/who-we-are/SolutionsSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <SolutionSection :items="SOLUTIONS_LIST" title="Keep exploring White Horse Labs" />
+</template>
+
+<script setup lang="ts">
+import SolutionSection from '@/components/pages/common/SolutionSection.vue'
+
+const SOLUTIONS_LIST = []
+</script>
