1669406d82c675534a49d82e8769bacfb31c29c0
vueexcel <vue.excel2011@gmail.com>
2024-07-29 15:36:56 +0530

    added explore section in news-details page.

--- src/views/resources/NewsDetailsView.vue
+++ src/views/resources/NewsDetailsView.vue
@@ -1,6 +1,7 @@
 <template>
     <main>
         <DetailsSection />
+        <ExploreMoreSection />
         <JoinusSection />
         <FooterSection />
     </main>
@@ -8,6 +9,7 @@
 
 <script setup lang="ts">
 import DetailsSection from '@/components/pages/resources/news-insight/DetailsSection.vue';
+import ExploreMoreSection from '@/components/pages/resources/news-insight/ExploreMoreSection.vue';
 import JoinusSection from '@/components/pages/common/JoinusSection.vue';
 import FooterSection from '@/components/pages/common/FooterSection.vue';
 </script>
