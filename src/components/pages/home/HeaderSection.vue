89b2642f28e14f92c9db1a671a67d0dd92d1152e
vueexcel <vue.excel2011@gmail.com>
2024-08-02 17:05:41 +0530

    completed homepage animation for big screens.

--- src/components/pages/home/HeaderSection.vue
+++ src/components/pages/home/HeaderSection.vue
@@ -1,12 +1,13 @@
 <template>
-    <section class="w-full h-[200vh] bg-black" ref="heroSection">
+    <section class="w-full h-[300vh] bg-black relative" ref="heroSection">
         <div class="h-screen w-full">
             <div class="flex flex-end flex-col w-full h-full bg-center bg-no-repeat bg-cover z-20"
                 :style="{ backgroundImage: `url(${heroImage})` }" ref="heroImage" />
         </div>
 
         <div class="w-full" ref="triggerBox" />
-        <div class="fixed top-0 w-full h-screen flex flex-col flex-end z-30">
+        <div class="top-0 w-full h-screen flex flex-col flex-end z-30"
+            :style="{ position: scrollY > (windowHeight * 0.5) ? 'static' : 'fixed' }">
             <div class="w-full mx-auto mt-auto max-w-[1350px] px-4 mb-8">
                 <h2 class="mb-32 text-6xl text-white" ref="heroTitle">
                     <span class="inline-block mb-2">Elevating confidence</span><br />
@@ -26,38 +27,38 @@
             </div>
         </div>
 
-        <div class="w-full h-screen z-10" style="transform: translateY(-100vh)" ref="featureSection">
-            <div class="w-full relative h-full bottom-0 flex items-center justify-between border">
+        <div class="w-full h-screen z-10 transition-none max-w-[100] overflow-hidden" :class="postionFromTop">
+            <div ref="imageSection" class="w-full relative h-full bottom-0 flex items-center justify-between">
                 <img :src="src.Image1" alt="images" class="absolute top-0 left-0 pointer-events-none" />
                 <img :src="src.Image2" alt="images" class="absolute top-0 right-0 pointer-events-none" />
                 <img :src="src.Image3" alt="images"
                     class="absolute pointer-events-none top-[50%] h-52 left-0 translate-y-[-20%]" />
                 <img :src="src.Image5" alt="images"
                     class="absolute pointer-events-none h-52 right-0 top-[50%] translate-y-[-20%]" />
-                <img :src="src.Image6" alt="images" class="absolute bottom-0 pointer-events-none left-[20%]" />
-                <img :src="src.Image7" alt="images" class="absolute bottom-0 pointer-events-none right-[20%]" />
+                <img :src="src.Image6" alt="images" class="absolute bottom-8 pointer-events-none left-[20%]" />
+                <img :src="src.Image7" alt="images" class="absolute bottom-5 pointer-events-none right-[20%]" />
+                <p ref="heroText"
+                    class="text-white opacity-0 mt-56 absolute left-[50%] translate-x-[-50%] text-center text-sm max-w-md sm:text-3xl px-6 sm:px-12">
+                    We safeguard supply chains with specialized testing, inspection, and logistics solutions,
+                    strategically integrated to ensure product integrity and reduce risk.
+                </p>
             </div>
         </div>
-
-        <!-- <p class="text-white mx-auto text-center text-sm max-w-md sm:text-3xl px-6 sm:px-12 opacity-0"
-            ref="featureTitle">
-            We safeguard supply chains with specialized testing, inspection, and logistics solutions,
-            strategically integrated to ensure product integrity and reduce risk.
-        </p> -->
     </section>
 </template>
 
 <script lang="ts">
 import gsap from 'gsap';
 import RedTitle from '@/components/common/RedTitle.vue';
+import ArrowIcon from '@/components/icons/ArrowIcon.vue';
 import MainImage from '@/assets/images/home/landingImage.png';
 
-import Image1 from '@/assets/images/home/Image-1.png'
-import Image2 from '@/assets/images/home/Image-2.png'
-import Image3 from '@/assets/images/home/Image-3.png'
-import Image5 from '@/assets/images/home/Image-5.png'
-import Image6 from '@/assets/images/home/Image-6.png'
-import Image7 from '@/assets/images/home/Image-7.png'
+import Image1 from '@/assets/images/home/Image-1.png';
+import Image2 from '@/assets/images/home/Image-2.png';
+import Image3 from '@/assets/images/home/Image-3.png';
+import Image5 from '@/assets/images/home/Image-5.png';
+import Image6 from '@/assets/images/home/Image-6.png';
+import Image7 from '@/assets/images/home/Image-7.png';
 
 export default {
     name: 'HeaderSection',
@@ -71,11 +72,35 @@ export default {
                 Image5,
                 Image6,
                 Image7
-            }
-        }
+            },
+            scrollY: 0,
+            windowHeight: window.innerHeight
+        };
     },
     components: {
-        RedTitle
+        RedTitle,
+        ArrowIcon
+    },
+    computed: {
+        postionFromTop() {
+            if (this.scrollY > this.windowHeight * 3) {
+                return '';
+            }
+
+            if (this.scrollY <= this.windowHeight * 2) {
+                return 'feature-image-1';
+            }
+
+            return ``;
+        }
+    },
+    methods: {
+        onScroll() {
+            this.scrollY = window.scrollY;
+        }
+    },
+    unmounted() {
+        window.removeEventListener('scroll', this.onScroll);
     },
     mounted() {
         const {
@@ -83,18 +108,21 @@ export default {
             ctaTitle,
             triggerBox,
             heroTitle,
-            featureTitle,
-            featureSection
+            imageSection,
+            heroText
         } = this.$refs as any;
 
+        const imgs = imageSection.querySelectorAll('img') as NodeListOf<HTMLImageElement>;
+
         gsap.to(heroImage, {
-            scale: 0.3,
+            scale: 0.25,
             transformOrigin: 'top center',
             scrollTrigger: {
                 trigger: heroImage,
                 scrub: true,
                 pinSpacing: false,
                 pin: true,
+                end: '+=1460px',
                 invalidateOnRefresh: true,
             }
         });
@@ -121,20 +149,158 @@ export default {
             }
         });
 
-        gsap.to(featureTitle, {
+        // image 1
+        gsap.fromTo(imgs[0],
+            {
+                x: "-100%",
+                y: "-40%",
+                scale: 1
+            },
+            {
+                x: "0%",
+                y: "0%",
+                scale: 1.2,
+                transformOrigin: 'bottom right',
+                scrollTrigger: {
+                    trigger: triggerBox,
+                    scrub: true,
+                    start: 'top bottom',
+                    end: '+=600',
+                    invalidateOnRefresh: true,
+                }
+            }
+        );
+
+        // image 2
+        gsap.fromTo(imgs[1],
+            {
+                x: "100%",
+                y: "-40%",
+                scale: 1
+            },
+            {
+                x: "0%",
+                y: "0%",
+                scale: 1.2,
+                transformOrigin: 'bottom left',
+                scrollTrigger: {
+                    trigger: triggerBox,
+                    scrub: true,
+                    start: 'top bottom',
+                    end: '+=600',
+                    invalidateOnRefresh: true,
+                }
+            }
+        );
+
+        // image 5
+        gsap.fromTo(imgs[2],
+            {
+                x: "-100%",
+                scale: 1
+            },
+            {
+                x: "0%",
+                scale: 1.2,
+                transformOrigin: 'top center',
+                scrollTrigger: {
+                    trigger: triggerBox,
+                    scrub: true,
+                    start: 'top bottom',
+                    end: '+=1000',
+                    invalidateOnRefresh: true,
+                }
+            }
+        );
+
+        // image 5
+        gsap.fromTo(imgs[3],
+            {
+                x: "100%",
+                scale: 1
+            },
+            {
+                x: "0%",
+                scale: 1.2,
+                transformOrigin: 'top center',
+                scrollTrigger: {
+                    trigger: triggerBox,
+                    scrub: true,
+                    start: 'top bottom',
+                    end: '+=1000',
+                    invalidateOnRefresh: true,
+                }
+            }
+        )
+
+        // image 5
+        gsap.fromTo(imgs[4],
+            {
+                x: "-220%",
+                y: "90%",
+                scale: 1
+            },
+            {
+                x: "0%",
+                y: "0%",
+                scale: 1.2,
+                transformOrigin: 'top right',
+                scrollTrigger: {
+                    trigger: triggerBox,
+                    scrub: true,
+                    start: 'top bottom',
+                    end: '+=1460',
+                    invalidateOnRefresh: true,
+                }
+            }
+        );
+
+        // image 5
+        gsap.fromTo(imgs[5],
+            {
+                x: "220%",
+                y: "90%",
+                scale: 1
+            },
+            {
+                x: "0%",
+                y: "0%",
+                scale: 1.2,
+                transformOrigin: 'top left',
+                scrollTrigger: {
+                    trigger: triggerBox,
+                    scrub: true,
+                    start: 'top bottom',
+                    end: '+=1660',
+                    invalidateOnRefresh: true,
+                }
+            }
+        );
+
+        gsap.fromTo(heroText, {
+            opacity: 0,
+            y: 100
+        }, {
             opacity: 1,
+            y: -100,
             scrollTrigger: {
                 trigger: triggerBox,
                 scrub: true,
-                start: 'top bottom',
-                end: '+=500',
+                start: '+=500px bottom',
+                end: '+=1200px',
                 invalidateOnRefresh: true,
             }
         })
 
-        gsap.to(featureSection, {
-            // 
-        })
+        window.addEventListener('scroll', this.onScroll);
     }
-}
+};
 </script>
+
+<style scoped>
+.feature-image-1 {
+    position: fixed;
+    top: 0;
+    left: 0;
+}
+</style>
