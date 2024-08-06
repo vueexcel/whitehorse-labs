6221f6926563f2d11b9c0ddbf79c97d8c4399ced
vueexcel <vue.excel2011@gmail.com>
2024-08-06 12:29:05 +0530

    refactor 'contact' page.

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,5 @@
 <template>
-    <InfoSection />
+    <EmptyHeaderSection title="Let’s Connect" />
     <FormSection />
     <LocationsSection />
     <JoinusSection />
@@ -7,9 +7,9 @@
 </template>
 
 <script setup lang="ts">
-import InfoSection from '@/components/pages/contact/InfoSection.vue';
 import FormSection from '@/components/pages/contact/FormSection.vue';
 import LocationsSection from '@/components/pages/contact/LocationsSection.vue';
 import JoinusSection from '@/components/pages/common/JoinusSection.vue';
 import FooterSection from '@/components/pages/common/FooterSection.vue';
+import EmptyHeaderSection from '@/components/pages/common/EmptyHeaderSection.vue';
 </script>
