f00ce478de19c4b24217b86ac1c463075c07ece3
vueexcel <vue.excel2011@gmail.com>
2024-08-08 16:24:48 +0530

    added hover effect.

--- src/components/pages/resources/NewsContentSection.vue
+++ src/components/pages/resources/NewsContentSection.vue
@@ -9,7 +9,7 @@
         <ul class="grid w-full grid-cols-1 gap-8 sm:grid-cols-2 xl:grid-cols-3">
             <li v-for="({ title, image }, index) in DUMMY_DATA" :key="index" class="w-full">
                 <router-link :to="'/resources/news-insight/' + (index + 1)">
-                    <ParallaxBackground class="w-full h-full" :src="image" :alt="'news-' + index" />
+                    <ParallaxBackground class="w-full h-auto" :src="image" :alt="'news-' + index" />
                     <h5 class="mt-3 text-sm text-black sm:text-md font-roboto max-">
                         {{ title }}
                     </h5>
