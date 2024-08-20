4273e15ae9ae41e1f2447c30f8b0960cf92a899e
vueexcel <vue.excel2011@gmail.com>
2024-08-20 10:00:23 +0530

    added copyright text at the footer.

--- src/components/pages/common/FooterSection.vue
+++ src/components/pages/common/FooterSection.vue
@@ -1,46 +1,29 @@
 <template>
   <section class="px-6 py-12 bg-black md:px-12 sm:pb-2" role="footer">
     <div
-      class="border-t border-t-[#828282] w-full pt-8 md:pt-16 pb-1 flex flex-col sm:flex-row items-center sm:items-start"
-    >
-      <router-link
-        to="/"
-        class="flex items-start justify-center h-auto mt-8 mb-20 sm:mt-0 sm:mb-0 sm:ml-5 w-36 sm:w-44 sm:justify-normal"
-      >
+      class="border-t border-t-[#828282] w-full pt-8 md:pt-16 pb-1 flex flex-col sm:flex-row items-center sm:items-start">
+      <router-link to="/"
+        class="flex items-start justify-center h-auto mt-8 mb-20 sm:mt-0 sm:mb-0 sm:ml-5 w-36 sm:w-44 sm:justify-normal">
         <img :src="Logo" alt="logo" class="h-28 sm:h-24" />
       </router-link>
       <div class="w-full max-w-full md:max-w-6xl">
         <div class="grid w-full grid-cols-1 gap-8 mb-12 sm:grid-cols-2 md:grid-cols-4 md:gap-4">
-          <div
-            class="flex flex-col items-start w-full text-white font-roboto"
-            v-for="menu in FooterLinks"
-            :key="menu.title"
-          >
+          <div class="flex flex-col items-start w-full text-white font-roboto" v-for="menu in FooterLinks"
+            :key="menu.title">
             <h6 class="mb-4 text-xl md:text-2xl font-roboto">{{ menu.title }}</h6>
-            <router-link
-              :to="`${menu.href}/${link.href}`"
-              class="text-[#828282] text-sm mb-3"
-              v-for="link in menu.links"
-              :key="link.name"
-              >{{ link.name }}</router-link
-            >
+            <router-link :to="`${menu.href}/${link.href}`" class="text-[#828282] text-sm mb-3"
+              v-for="link in menu.links" :key="link.name">{{ link.name }}</router-link>
           </div>
         </div>
-        <div
-          class="flex flex-col items-center justify-between w-full mt-10 mb-3 text-white sm:flex-row md:mt-20"
-        >
+        <div class="flex flex-col items-center justify-between w-full mt-10 mb-3 text-white sm:flex-row md:mt-20">
           <p class="text-[#828282] text-xs md:text-sm text-center md:text-left mb-4 md:mb-0">
-            © 2024 White Horse Laboratories. All rights reserved.
+            © 2024 White Horse Laboratories. All rights reserved : Website by <a href="http://www.wiseandhype.com/"
+              class="text-red-500 hover:opacity-75" target="_blank"> Wise+Hype </a>
           </p>
           <div class="flex items-center">
-            <router-link
-              :to="socialLink.href"
-              class="mx-2"
-              :title="socialLink.title"
-              v-for="socialLink in socialLinks"
-              :key="socialLink.name"
-            >
-              <component :is="socialLink.icon" class="size-5" path-class="fill-slate-400" />
+            <router-link :to="socialLink.href" class="mx-2" :title="socialLink.title" v-for="socialLink in socialLinks"
+              :key="socialLink.name">
+              <component :is="socialLink.icon" class="size-5" path-class="fill-[#828282]" />
             </router-link>
           </div>
         </div>
