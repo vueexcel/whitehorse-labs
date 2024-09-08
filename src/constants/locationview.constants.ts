52435e9e53be393a068d24880c90eee4ae152a6d
vueexcel <vue.excel2011@gmail.com>
2024-09-08 17:50:13 +0530

    fixed

--- src/constants/locationview.constants.ts
+++ src/constants/locationview.constants.ts
@@ -1,30 +1,98 @@
-const EN_LANG = {
-    // 
-}
+import HeroImage from '@/assets/images/locations/hero.png'
+import TopImage from '@/assets/images/locations/top.png'
+import WiderImage from '@/assets/images/locations/wider.png'
+import MapImage from '@/assets/images/locations/map.png'
+
+import DE_ServiceImage1 from '@/assets/images/locations/service-1.png'
+import DE_ServiceImage2 from '@/assets/images/locations/service-2.png'
+import DE_ServiceImage3 from '@/assets/images/locations/service-3.png'
+import DE_ServiceImage4 from '@/assets/images/locations/service-4.png'
+import DE_ServiceImage5 from '@/assets/images/locations/service-5.png'
+import DE_ServiceImage6 from '@/assets/images/locations/service-6.png'
 
 const DE_LANG = {
     heroSection: {
-        title: ['The Oskar von Miller Test Centre', 'in Munich, Germany'],
-        description: 'The Oskar von Miller Test Centre is committed to upholding the ISO17025 standard, following our established accredited testing facilities in Shenzhen, China, and Cleveland, USA.',
-    } 
+        subtitle: 'global guardians of technological integrity',
+        title: ['The Oskar von', 'Miller Test Centre', 'in Munich, Germany'],
+        paragraph: 'The Oskar von Miller Test Centre is committed to upholding the ISO17025 standard, following our established accredited testing facilities in Shenzhen, China, and Cleveland, USA.',
+        backgroundImage: HeroImage
+    },
+    sectionHeader: {
+        subtitle: 'Munich, Germany',
+        title: ['Precision Testing for Europe’s Advanced', 'Sectors'],
+        paragraph: `White Horse Laboratories, a global leader in ensuring the integrity of electronics, proudly
+            offers its cutting-edge European test lab in Munich, Germany. Open for over a year, this
+            facility seamlessly integrates our renowned industry expertise with a focus on rapid, precise
+            testing, delivering an exceptional service experience to our customers.
+            <br><br> <q>Integrity and expertise are the foundation of our services,</q> says Mark Rinehart,
+            Founder and Managing Director. The Munich lab exemplifies our commitment to providing fast,
+            seamless testing processes with clear, actionable results, upholding our promise of customer
+            satisfaction.`,
+        image: TopImage
+    },
+    serviceSection: {
+        link: '#',
+        title: 'Our capabilites',
+        content: [
+            {
+                title: 'Component Testing and Counterfeit Detection',
+                list: 'Understanding the critical importance of time in the electronics industry, we guarantee swift yet thorough analysis. Our detailed, easy-to-understand test reports provide precise insights into the condition of your components, identifying potential counterfeits with the utmost efficiency.',
+                description:
+                    'Our expert team performs rigorous component testing using advanced visual inspection, mechanical testing, X-ray analysis, and more. We ensure the quality and functionality of your electronic components, reliably detecting counterfeit or substandard parts. Our testing services strictly adhere to industry standards like AS6081/AS6171, delivering consistently reliable and high-quality results.',
+                images: [DE_ServiceImage1, DE_ServiceImage2]
+            },
+            {
+                title: 'Failure Analysis',
+                description: 'Our thorough failure analysis uncovers potential issues within your electronic components, providing targeted solutions to address them effectively. Leveraging cutting-edge technology and the expertise of seasoned industry professionals, we excel in detecting internal failures, assessing processing materials, and conducting in-process quality controls to ensure the highest level of reliability.',
+                list: 'At White Horse Laboratories, our comprehensive failure analysis is designed to quickly identify problems and deliver solutions tailored to your unique needs. As proud members of FED, COGD, and ERAI, we are committed to upholding the highest standards of excellence and integrity in every aspect of our failure analysis services.',
+                images: [DE_ServiceImage3, DE_ServiceImage4]
+            },
+            {
+                title: 'Component Programming, Packaging, and Logistics',
+                description: 'Our service begins with a detailed review of your products’ current state, followed by complete processing and packaging of your electronic components tailored to your production needs. From repackaging to custom labeling, we ensure your supply chain runs smoothly and efficiently.',
+                list: 'To optimize your components\' performance, we offer customized programming services aligned with your specific requirements. Our experts deliver precise programming that ensures your components are fully prepared for their intended application, enhancing functionality and reliability.',
+                images: [DE_ServiceImage5, DE_ServiceImage6]
+            }
+        ]
+    },
+    widerBox: {
+        title: 'Quality is Our Commitment',
+        paragraph: `At White Horse Laboratories, we are dedicated to safeguarding global supply chains by utilizing state-of-the-art
+            testing equipment and both industry-standard and proprietary testing methods.
+            <br><br>What truly sets us apart are two core principles: an unyielding commitment to integrity and a collaborative
+            approach with our clients. These guiding values enable us to craft bespoke solutions tailored to your specific
+            needs, ensuring your success every step of the way.`,
+        image: WiderImage
+    },
+    calendarSection: {
+        title: 'Calendar',
+        subtitle: 'Stay Connected: Upcoming Events & Key Dates',
+        paragraph: `Trust White Horse Labs for swift, reliable service to maintain a resilient supply chain. Streamline
+            your order planning with updated hours from our Munich office.`,
+        calendarUrl: 'https://outlook.office.com/calendar/published/4c9c7d58703e49c99f663824d6e3bdba@whitehorselabs.com/533e7bda0315429d922e8e9bd3122e015689917153247656081/calendar.html'
+    },
+    contactSection: {
+        title: 'Contact',
+        subtitle: [' White Horse Laboratories', 'GmbH'],
+        paragraph: [
+            'Lindberghstraße 12',
+            '82178 Puchheim',
+            'Germany',
+            'Telephone: +49 89 541 973 12',
+            'Email: <a href="mailto:info@whitehorselabs.com" class="text-[#FF544F] underline hover:opacity-75">info@whitehorselabs.com</a>',
+        ],
+        directionLink: '#',
+        image: MapImage
+    }
 }
 
-const IN_LANG = {
-    // 
-}
-
-const CN_LANG = {
-    // 
-}
-
-const SG_LANG = {
-    // 
-}
+export type Language = 'en' | 'de' | 'in' | 'cn' | 'sg';
+export type LocationView = typeof DE_LANG;
 
 export default {
-    en: EN_LANG,
+    en: DE_LANG,
     de: DE_LANG,
-    in: IN_LANG,
-    cn: CN_LANG,
-    sg: SG_LANG
+    in: DE_LANG,
+    cn: DE_LANG,
+    sg: DE_LANG
 }
--- src/views/LocationView.vue
+++ src/views/LocationView.vue
@@ -1,39 +1,31 @@
 <template>
     <main>
-        <HeroSection title="global guardians of technological integrity" :background-image="HeroImage">
-            The Oskar von <br class="hidden md:block"> Miller Test Centre<br class="hidden md:block"> <span
-                class="font-Tiempos-Headline">in Munich, Germany</span>
+        <HeroSection :title="content.heroSection.subtitle" :background-image="content.heroSection.backgroundImage">
+            <span v-html="content.heroSection.title.slice(0, 1).join(`<br class='hidden md:block'></span>`)"></span>
+            <span class="font-Tiempos-Headline">{{ content.heroSection.title.slice(-1)[0] }}</span>
+
             <template #description>
-                The Oskar von Miller Test Centre is committed to upholding the ISO17025 standard, following our
-                established accredited testing facilities in Shenzhen, China, and Cleveland, USA.
+                {{ content.heroSection.paragraph }}
             </template>
         </HeroSection>
 
-        <SectionHeader title="Munich, Germany" class="bg-white" :header-shown="false" top-bottom-class="w-full max-w-6xl">
+        <SectionHeader :title="content.sectionHeader.subtitle" class="bg-white" :header-shown="false" top-bottom-class="w-full max-w-6xl">
             <template #header></template>
             <template #top>
                 <div class="w-full flex-1">
                     <h4 class="mb-5 max-w-sm mt-4 text-2xl text-black sm:text-3xl font-roboto sm:mt-0"> Precision
-                        Testing for Europe’s Advanced <br class="hidden md:block"> Sectors </h4>
-                    <p class="text-[#828282] font-roboto max-w-md">
-                        White Horse Laboratories, a global leader in ensuring the integrity of electronics, proudly
-                        offers its cutting-edge European test lab in Munich, Germany. Open for over a year, this
-                        facility seamlessly integrates our renowned industry expertise with a focus on rapid, precise
-                        testing, delivering an exceptional service experience to our customers.
-                        <br><br> <q>Integrity and expertise are the foundation of our services,</q> says Mark Rinehart,
-                        Founder and Managing Director. The Munich lab exemplifies our commitment to providing fast,
-                        seamless testing processes with clear, actionable results, upholding our promise of customer
-                        satisfaction.
-                    </p>
+                        {{ content.sectionHeader.title.join('<br class="hidden md:block">') }}
+                    </h4>
+                    <p class="text-[#828282] font-roboto max-w-md" v-html="content.sectionHeader.paragraph"></p>
                 </div>
 
-                <img :src="TopImage" alt="top" class="h-full object-cover pointer-events-none max-w-sm sm:ml-auto" />
+                <img :src="content.sectionHeader.image" alt="top" class="h-full object-cover pointer-events-none max-w-sm sm:ml-auto" />
             </template>
         </SectionHeader>
 
         <CompanySection />
-        <ServicesSection cta="" cta-link="#" title="Our capabilites" :items="SERVICES_LIST" />
-        <WiderBox :background-image="WiderImage"  class="bg-white">
+        <ServicesSection cta="" :cta-link="content.serviceSection.link" :title="content.serviceSection.title" :items="content.serviceSection.content" />
+        <WiderBox :background-image="content.widerBox.image"  class="bg-white">
             <template #title>
                 Quality is Our Commitment
             </template>
@@ -48,40 +40,32 @@
             needs, ensuring your success every step of the way.
         </WiderBox>
 
-        <SectionHeader title="Calendar" subtitle="Stay Connected: Upcoming Events & Key Dates" class="bg-white">
+        <SectionHeader :title="content.calendarSection.title" :subtitle="content.calendarSection.subtitle" class="bg-white">
             <template #top>
                 <p class="text-[#828282] font-roboto">
-                    Trust White Horse Labs for swift, reliable service to maintain a resilient supply chain. Streamline
-                    your order planning with updated hours from our Munich office.
+                    {{ content.calendarSection.paragraph }}
                 </p>
             </template>
         </SectionHeader>
         <section class="w-full px-6 mx-auto bg-white pt-12 pb-20 flex items-center justify-center">
-            <iframe class="w-full max-w-6xl" :src="calendarUrl" style="border: 0" width="100%" height="600"
+            <iframe class="w-full max-w-6xl" :src="content.calendarSection.calendarUrl" style="border: 0" width="100%" height="600"
                 frameborder="0" scrolling="no"></iframe>
         </section>
 
-        <SectionHeader title="Contact" :header-shown="false" class="bg-white" top-bottom-class="w-full max-w-7xl">
+        <SectionHeader :title="content.contactSection.title" :header-shown="false" class="bg-white" top-bottom-class="w-full max-w-7xl">
             <template #header></template>
             <template #top>
                 <div class="w-full flex-1 max-w-lg">
-                    <h4 class="mb-5 max-w-sm mt-4 text-2xl text-black sm:text-3xl font-roboto sm:mt-0">
-                        White Horse Laboratories <br class="hidden md:block"> GmbH </h4>
+                    <h4 class="mb-5 max-w-sm mt-4 text-2xl text-black sm:text-3xl font-roboto sm:mt-0" v-html="content.contactSection.subtitle.join(`<br class='hidden md:block'>`)"></h4>
                     <p class="font-roboto text-[#828282] sm:text-lg">
-                        Lindberghstraße 12 <br>
-                        82178 Puchheim <br>
-                        Germany<br><br>
-                        Telephone: +49 89 541 973 12<br>
-                        Email: <a href="mailto:info@whitehorselabs.com"
-                            class="text-[#FF544F] underline hover:opacity-75">info@whitehorselabs.com</a><br>
-                        <br>
-                        <a href="#" class="text-[#FF544F] underline hover:opacity-75">
+                        <span v-html="content.contactSection.paragraph.join('<br>')"></span>
+                        <a :href="content.contactSection.directionLink" class="text-[#FF544F] underline hover:opacity-75">
                             Directions
                         </a>
                     </p>
                 </div>
 
-                <img :src="MapImage" alt="top" class="w-full object-cover pointer-events-none max-w-3xl sm:ml-auto" />
+                <img :src="content.contactSection.image" alt="top" class="w-full object-cover pointer-events-none max-w-3xl sm:ml-auto" />
             </template>
         </SectionHeader>
 
@@ -105,40 +89,15 @@ import FormSection from '@/components/pages/contact/FormSection.vue';
 import JoinusSection from '@/components/pages/common/JoinusSection.vue';
 import FooterSection from '@/components/pages/common/FooterSection.vue';
 
-import HeroImage from '@/assets/images/locations/hero.png'
-import TopImage from '@/assets/images/locations/top.png'
-import WiderImage from '@/assets/images/locations/wider.png'
-import MapImage from '@/assets/images/locations/map.png'
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import ContentData, { type Language, type LocationView } from '@/constants/locationview.constants'
 
-import ServiceImage1 from '@/assets/images/locations/service-1.png'
-import ServiceImage2 from '@/assets/images/locations/service-2.png'
-import ServiceImage3 from '@/assets/images/locations/service-3.png'
-import ServiceImage4 from '@/assets/images/locations/service-4.png'
-import ServiceImage5 from '@/assets/images/locations/service-5.png'
-import ServiceImage6 from '@/assets/images/locations/service-6.png'
+const route = useRoute();
+const language = route.params.id as Language
+const content = ref((ContentData[language as Language] || ContentData.en) as LocationView)
 
-const SERVICES_LIST = [
-    {
-        title: 'Component Testing and Counterfeit Detection',
-        list: 'Understanding the critical importance of time in the electronics industry, we guarantee swift yet thorough analysis. Our detailed, easy-to-understand test reports provide precise insights into the condition of your components, identifying potential counterfeits with the utmost efficiency.',
-        description:
-            'Our expert team performs rigorous component testing using advanced visual inspection, mechanical testing, X-ray analysis, and more. We ensure the quality and functionality of your electronic components, reliably detecting counterfeit or substandard parts. Our testing services strictly adhere to industry standards like AS6081/AS6171, delivering consistently reliable and high-quality results.',
-        images: [ServiceImage1, ServiceImage2]
-    },
-    {
-        title: 'Failure Analysis',
-        description: 'Our thorough failure analysis uncovers potential issues within your electronic components, providing targeted solutions to address them effectively. Leveraging cutting-edge technology and the expertise of seasoned industry professionals, we excel in detecting internal failures, assessing processing materials, and conducting in-process quality controls to ensure the highest level of reliability.',
-        list: 'At White Horse Laboratories, our comprehensive failure analysis is designed to quickly identify problems and deliver solutions tailored to your unique needs. As proud members of FED, COGD, and ERAI, we are committed to upholding the highest standards of excellence and integrity in every aspect of our failure analysis services.',
-        images: [ServiceImage3, ServiceImage4]
-    },
-    {
-        title: 'Component Programming, Packaging, and Logistics',
-        description: 'Our service begins with a detailed review of your products’ current state, followed by complete processing and packaging of your electronic components tailored to your production needs. From repackaging to custom labeling, we ensure your supply chain runs smoothly and efficiently.',
-        list: 'To optimize your components\' performance, we offer customized programming services aligned with your specific requirements. Our experts deliver precise programming that ensures your components are fully prepared for their intended application, enhancing functionality and reliability.',
-        images: [ServiceImage5, ServiceImage6]
-    }
-]
-
-const calendarUrl =
-    'https://outlook.office.com/calendar/published/4c9c7d58703e49c99f663824d6e3bdba@whitehorselabs.com/533e7bda0315429d922e8e9bd3122e015689917153247656081/calendar.html'
+watch(() => route.params.id, () => {
+    content.value = (ContentData[language as Language] || ContentData.en) as LocationView
+})
 </script>
