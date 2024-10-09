bc875279e9f5e192f7734dd62fb5a7b0d2e8b78d
vueexcel <vue.excel2011@gmail.com>
2024-10-09 23:19:58 +0530

    added location page in/ch

--- src/constants/locationview.constants.ts
+++ src/constants/locationview.constants.ts
@@ -14,6 +14,11 @@ import SP_WiderImage from '@/assets/images/locations/SP_wider.png'
 import SP_MapImage from '@/assets/images/locations/SP_map.png'
 import SP_video from '@/assets/images/locations/SP_video.png'
 
+import IN_HeroImage from '@/assets/images/locations/IN_hero.png'
+import IN_TopImage from '@/assets/images/locations/IN_top.png'
+import IN_WiderImage from '@/assets/images/locations/IN-wider.png'
+import IN_MapImage from '@/assets/images/locations/IN_map.png'
+
 import DE_ServiceImage1 from '@/assets/images/locations/service-1.png'
 import DE_ServiceImage2 from '@/assets/images/locations/service-2.png'
 import DE_ServiceImage3 from '@/assets/images/locations/service-3.png'
@@ -25,9 +30,14 @@ import CH_ServiceImage1 from '@/assets/images/locations/ch_service-1.png'
 import CH_ServiceImage2 from '@/assets/images/locations/ch_service-2.png'
 import CH_ServiceImage3 from '@/assets/images/locations/ch_service-3.png'
 import CH_ServiceImage4 from '@/assets/images/locations/ch_service-4.png'
+import CH_ServiceImage5 from '@/assets/images/locations/ch_service-5.png'
+import CH_ServiceImage6 from '@/assets/images/locations/ch_service-6.png'
 
 import SP_ServiceImage1 from '@/assets/images/locations/SP_service-1.png'
 import SP_ServiceImage2 from '@/assets/images/locations/SP_service-2.png'
+
+import IN_ServiceImage1 from '@/assets/images/locations/IN_service-1.png'
+import IN_ServiceImage2 from '@/assets/images/locations/IN_service-2.png'
 
 const DE_LANG = {
     heroSection: {
@@ -108,15 +118,15 @@ const DE_LANG = {
 const CH_LANG = {
     heroSection: {
         subtitle: 'global guardians of technological integrity',
-        title: ['The William', 'Shockley Testing', 'Centre in Shenzhen'],
-        paragraph : 'The William Shockley Testing Centre: A Global Leader in Testing and Operations, ISO 17025:2015 Accredited for Unmatched Quality and Reliability in Asia',  
+        title: ['Unmatched Quality', 'and Reliability in the', 'heart of Asia'],
+        paragraph : 'The William Shockley Testing Centre and Ted Hoff Testing and Logistics Centre are the leading global facilites for testing and operations, ISO 17025:2015 accredited for technological reliability in asia',  
         backgroundImage: CH_HeroImage
     },
     sectionHeader: {
-        subtitle: 'Shenzhen, China',
-        title: ['Unmatched Quality and Reliability in the',' heart of Asia'],
+        subtitle: 'The heart of asia',
+        title: ['Located in Hong Kong and Shenzhen to better serve you'],
         paragraph: `Founded in 2004, White Horse Labs made Shenzhen our first home, and nearly two decades later, it has grown into our global headquarters. With over 120 employees—more than half dedicated to operations—our Shenzhen facility is a hub of excellence in testing, operations, and customer service.
-            <br><br> <q>Spanning over 2,000 square meters, the lab offers comprehensive AS6081 testing and advanced electrical tests to meet the highest industry standards. In 2023, the William Shockley Testing Centre at this facility earned ISO 17025:2015 accreditation, reaffirming our commitment to quality, precision, and reliability across the supply chain.`,
+            <br><br> <q>Spanning over 4,000 total square meters, the labs offers comprehensive AS6081 testing and advanced electrical tests to meet the highest industry standards. In 2023, the William Shockley Testing Centre at this facility earned ISO 17025:2015 accreditation, reaffirming our commitment to quality, precision, and reliability across the supply chain.`,
         image: CH_TopImage
     },
     serviceSection: {
@@ -131,9 +141,15 @@ const CH_LANG = {
                 images: [CH_ServiceImage1, CH_ServiceImage2]
             },
             {
-                title: 'Our Shenzhen Team: Building Expertise, Driving Growth',
-                description :'Established in 2004, the White Horse Labs Shenzhen team brings together lab technicians, administration, sales, and customer service under a family-oriented culture. Many of our team members have been with us for over a decade, reflecting our commitment to long-term growth and a supportive work environment.',
-                list : 'With more than 60 highly skilled technicians and experienced engineers, our Shenzhen operations team leads the way in quality and precision. Over the years, this team has also played a key role in training and supporting our new labs in Singapore and Cleveland, sharing their expertise to ensure success across all our locations.',
+                title : 'Ted Hoff Testing & Logistics Centre: Quality Control and Logistics',
+                description : `The Ted Hoff Testing & Logistics Centre in Hong Kong is a key facility in White Horse Labs' global network, certified to ISO/IEC 17025:2017 standards. It operates as a 3PL logistics hub, providing seamless handling, storage, and distribution services to ensure efficient product flow. With a focus on maintaining the highest standards of quality and reliability, the centre plays a crucial role in supporting our clients' supply chains by offering streamlined logistics solutions.`,
+                list : 'n addition to its logistics capabilities, the centre is equipped to perform the full AS6081 and AS6171 test protocols, vital for detecting counterfeit and substandard components. This ensures that all electronic parts meet industry standards and are safe for use. The facility also offers advanced electrical testing, guaranteeing the performance and reliability of electronic components. Together, these services allow the Ted Hoff Testing & Logistics Centre to deliver precision and trust to customers worldwide.',
+                images : [CH_ServiceImage5, CH_ServiceImage6]
+            },
+            {
+                title: 'Our Asia Team: Building Expertise, Driving Growth',
+                description :'The White Horse Labs Asia team brings together lab technicians, administration, sales, and customer service under a family-oriented culture. Many of our team members have been with us for over a decade, reflecting our commitment to long-term growth and a supportive work environment.',
+                list : 'With nearly 100 highly skilled technicians and experienced engineers, our Asia operations team leads the way in quality and precision. Over the years, this team has also played a key role in training and supporting our new labs in Singapore and Cleveland, sharing their expertise to ensure success across all our locations.',
                 images: [CH_ServiceImage3, CH_ServiceImage4]
             },
         ]
@@ -235,15 +251,67 @@ const SP_LANG = {
     }
 }
 
+const IN_LANG = {
+    heroSection: {
+        subtitle: 'global guardians of technological integrity',
+        title: ['Driving Growth ', 'and Excellence ', 'in India'],
+        paragraph : 'The White Horse Labs India facility drives business growth and delivers exceptional customer service, ensuring strong global partnerships and seamless support.',  
+        backgroundImage: IN_HeroImage
+    },
+    sectionHeader: {
+        subtitle: 'india',
+        title: ['Customer Focus and Growth: White Horse Labs , India'],
+        paragraph: `The White Horse Labs India facility is a cornerstone for our business development and customer service efforts, strategically positioned to support both local and international clients. This location is dedicated to building strong client relationships and ensuring smooth communication across the entire supply chain. Our team in India excels in delivering exceptional customer service while driving growth and expanding White Horse Labs' presence in the region.
+            <br><br> <q>Certified with the Indian Graphic Trademark Certificate, IPC Membership Certificate, COGD Membership Certification, and ERAI Member Certificate, the India facility upholds our commitment to quality and ethical practices. These certifications reflect our unwavering dedication to providing trusted, industry-leading services for our clients across the globe.`,
+        image: IN_TopImage
+    },
+    serviceSection: {
+        link: '#',
+        title: 'Our capabilites',
+        content: [
+            {
+                title: 'The Gordon E. Moore Testing Centre: Advancing with Innovation',
+                list : 'Alongside this, we ensure top-tier customer service, providing responsive and reliable support to maintain seamless operations and long-term client satisfaction. This combination of strategic development and dedicated service makes the India facility a vital part of our global operations.',
+                description:
+                    'The White Horse Labs India facility focuses on two primary capabilities: business development and customer service. Our team actively supports clients by identifying growth opportunities, fostering partnerships, and expanding our reach in the region. ',
+                images: [IN_ServiceImage1, IN_ServiceImage2]
+            },
+        ]
+    },
+    widerBox: {
+        title: 'Quality is Our Commitment',
+        paragraph: `At White Horse Laboratories, we are dedicated to safeguarding global supply chains by utilizing state-of-the-art testing equipment and both industry-standard and proprietary testing methods.
+            <br><br>What truly sets us apart are two core principles: an unyielding commitment to integrity and a collaborative approach with our clients. These guiding values enable us to craft bespoke solutions tailored to your specific needs, ensuring your success every step of the way.`,
+        image: IN_WiderImage
+    },
+    calendarSection: {
+        title: 'Calendar',
+        subtitle: 'Stay Connected: Upcoming Events & Key Dates',
+        paragraph: `Trust White Horse Labs for swift, reliable service to maintain a resilient supply chain. Streamline your order planning with updated hours from our India office.`,
+        calendarUrl: 'https://outlook.office.com/calendar/published/4c9c7d58703e49c99f663824d6e3bdba@whitehorselabs.com/533e7bda0315429d922e8e9bd3122e015689917153247656081/calendar.html'
+    },
+    contactSection: {
+        title: 'Contact',
+        subtitle: [' White Horse Laboratories', 'India'],
+        paragraph: [
+            '#1315 Ardente Office One ',
+            'Tower A, 3rd Floor, Hoody Circle,',
+            'Bangalore 560048, Karnataka, India',
+            // 'Phone: +65 6539 9638',
+            'Email: <a href="mailto:info@whitehorselabs.com" class="text-[#FF544F] underline hover:opacity-75">info@whitehorselabs.com</a>',
+        ],
+        directionLink: '#',
+        image: IN_MapImage
+    }
+}
+
 export type Language = 'en' | 'de' | 'in' | 'cn' | 'sg';
 export type LocationView = typeof DE_LANG
 
 export default {
-    // en: CH_LANG,
-    // en: DE_LANG,
     en:DE_LANG,
     de: DE_LANG,
-    in: DE_LANG,
+    in: IN_LANG,
     cn: CH_LANG,
     sg: SP_LANG
 }
