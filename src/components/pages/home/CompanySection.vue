36879a36c944026702fa5acf0ab3311d952715d4
vueexcel <vue.excel2011@gmail.com>
2025-01-31 17:24:22 +0530

    added-logo/change-siteLogo

--- src/components/pages/home/CompanySection.vue
+++ src/components/pages/home/CompanySection.vue
@@ -1,13 +1,13 @@
 <template>
   <SectionHeader title="CONNECTED AND COMPLIANT" subtitle="" class="pb-0 bg-white">
     <template #topBottom>
-      <ul class="grid w-full grid-cols-2 gap-8 -translate-y-8 md:grid-cols-3 lg:grid-cols-6">
+      <ul class="grid w-full grid-cols-2 gap-8 -translate-y-8 md:grid-cols-3 lg:grid-cols-7">
         <li
           v-for="{ link, image, name, class: cls } in COMPANYS"
           :key="link"
           class="flex items-center justify-center transition hover:opacity-80"
         >
-          <router-link :to="link" :title="name" class="flex items-center justify-center">
+          <router-link :title="name" class="flex items-center justify-center">
             <img :src="image" :alt="name" :class="cls" class="w-auto" />
           </router-link>
         </li>
@@ -19,12 +19,13 @@
 <script setup lang="ts">
 import SectionHeader from '@/components/layout/SectionHeader.vue'
 
-import Company1 from '@/assets/images/home/esd.png'
-import Company2 from '@/assets/images/home/ipc.png'
-import Company3 from '@/assets/images/home/anab.png'
-import Company4 from '@/assets/images/home/cogd.png'
-import Company5 from '@/assets/images/home/erai.png'
-import Company6 from '@/assets/images/home/tuv.png'
+import Company1 from '@/assets/images/home/esd.svg'
+import Company2 from '@/assets/images/home/ipc.svg'
+import Company3 from '@/assets/images/home/anab.svg'
+import Company4 from '@/assets/images/home/cogd.svg'
+import Company5 from '@/assets/images/home/erai.svg'
+import Company6 from '@/assets/images/home/tuv.svg'
+import Company7 from '@/assets/images/home/dla.svg'
 
 const COMPANYS = [
   {
@@ -61,6 +62,12 @@ const COMPANYS = [
     link: '#',
     image: Company6,
     name: 'Tuv',
+    class: 'h-24'
+  },
+  {
+    link: '#',
+    image: Company7,
+    name: 'dla',
     class: 'h-24'
   }
 ]
--- src/components/pages/locations/CompanySection.vue
+++ src/components/pages/locations/CompanySection.vue
@@ -1,7 +1,7 @@
 <template>
     <SectionHeader title="Certifications" subtitle="" class="pb-0 bg-white">
       <template #topBottom>
-        <ul class="grid w-full grid-cols-2 gap-8 -translate-y-8 md:grid-cols-3 lg:grid-cols-6">
+        <ul class="grid w-full grid-cols-2 gap-8 -translate-y-8 md:grid-cols-3 lg:grid-cols-7">
           <li
             v-for="{ link, image, name, class: cls } in COMPANYS"
             :key="link"
@@ -19,12 +19,13 @@
   <script setup lang="ts">
   import SectionHeader from '@/components/layout/SectionHeader.vue'
   
-  import Company1 from '@/assets/images/home/esd.png'
-  import Company2 from '@/assets/images/home/ipc.png'
-  import Company3 from '@/assets/images/home/anab.png'
-  import Company4 from '@/assets/images/home/cogd.png'
-  import Company5 from '@/assets/images/home/erai.png'
-  import Company6 from '@/assets/images/home/tuv.png'
+  import Company1 from '@/assets/images/home/esd.svg'
+  import Company2 from '@/assets/images/home/ipc.svg'
+  import Company3 from '@/assets/images/home/anab.svg'
+  import Company4 from '@/assets/images/home/cogd.svg'
+  import Company5 from '@/assets/images/home/erai.svg'
+  import Company6 from '@/assets/images/home/tuv.svg'
+  import Company7 from '@/assets/images/home/dla.svg'
   
   const COMPANYS = [
     {
@@ -63,6 +64,12 @@
       name: 'Anab',
       class: 'h-24'
     },
+    {
+      link: '#',
+      image: Company7,
+      name: 'dla',
+      class: 'h-24'
+    }
   ]
   </script>
   
