1ca4df9b67513bcccf4e5ebe504718f00514806f
vueexcel <vue.excel2011@gmail.com>
2025-02-05 12:07:22 +0530

    fix-logo/multilanguage-header

--- src/components/pages/home/CompanySection.vue
+++ src/components/pages/home/CompanySection.vue
@@ -2,13 +2,13 @@
   <SectionHeader title="CONNECTED AND COMPLIANT" subtitle="" class="pb-0 bg-white">
     <template #topBottom>
       <ul class="grid w-full grid-cols-2 gap-8 -translate-y-8 md:grid-cols-3 lg:grid-cols-7">
-        <li
-          v-for="{ link, image, name, class: cls } in COMPANYS"
-          :key="link"
-          class="flex items-center justify-center transition hover:opacity-80"
-        >
+        <li v-for="{ link, image, name, class: cls }, index in COMPANYS" :key="link"
+          class="flex items-center justify-center transition hover:opacity-80">
           <div class="flex items-center justify-center">
-            <img :src="image" :alt="name" :class="cls" class="w-auto" />
+            <a v-if="index === 1" :href="link" target="_blank" rel="noopener noreferrer">
+              <img :src="image" :alt="name" :class="cls + ' hover:opacity-80 cursor-pointer'" title="click to open" class="w-auto" />
+            </a>
+            <img v-else :src="image" :alt="name" :class="cls" class="w-auto" />
           </div>
         </li>
       </ul>
@@ -18,6 +18,7 @@
 
 <script setup lang="ts">
 import SectionHeader from '@/components/layout/SectionHeader.vue'
+import WHL_PDF from '@/assets/WHL_suite_letter.pdf'
 
 import Company1 from '@/assets/images/home/esd.svg'
 import Company2 from '@/assets/images/home/ipc.svg'
@@ -28,6 +29,18 @@ import Company6 from '@/assets/images/home/tuv.svg'
 import Company7 from '@/assets/images/home/dla.svg'
 
 const COMPANYS = [
+  {
+    link: '#',
+    image: Company6,
+    name: 'Tuv',
+    class: 'h-24'
+  },
+  {
+    link: WHL_PDF,
+    image: Company7,
+    name: 'dla',
+    class: 'h-24'
+  },
   {
     link: '#',
     image: Company1,
@@ -58,17 +71,5 @@ const COMPANYS = [
     name: 'Erai',
     class: 'h-24'
   },
-  {
-    link: '#',
-    image: Company6,
-    name: 'Tuv',
-    class: 'h-24'
-  },
-  {
-    link: '#',
-    image: Company7,
-    name: 'dla',
-    class: 'h-24'
-  }
 ]
 </script>
